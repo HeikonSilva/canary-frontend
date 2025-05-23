b42ba2678ad94a9f4e2e4e5cd25545fdb93b166a
HeikonSilva <heikon.costa@etec.sp.gov.br>
2025-05-23 13:51:37 -0300

    Refactor project structure and update styles
    - Removed global.css and migrated styles to index.css.
    - Updated main.tsx to import index.css and wrapped the application in StrictMode.
    - Deleted unused server routes and server setup files.
    - Added vite-env.d.ts for Vite support.

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router'
 import { createRoot } from 'react-dom/client'
-import './global.css'
+import './index.css'
 import Layout from './components/layout'
 import Home from './pages/Home'
 import Servers from './pages/Servers'
@@ -9,20 +9,23 @@ import AuthLayout from './components/authLayout'
 import SignUp from './pages/SignUp'
 import SignIn from './pages/SignIn'
 import ForgotPassord from './pages/ForgotPassword'
+import { StrictMode } from 'react'
 
 createRoot(document.getElementById('root')!).render(
-  <BrowserRouter>
-    <Routes>
-      <Route index element={<Landing />} />
-      <Route element={<AuthLayout />}>
-        <Route path="signup" element={<SignUp />} />
-        <Route path="signin" element={<SignIn />} />
-        <Route path="forgotpassword" element={<ForgotPassord />} />
-      </Route>
-      <Route element={<Layout />}>
-        <Route path="home" element={<Home />} />
-        <Route path="servers" element={<Servers />} />
-      </Route>
-    </Routes>
-  </BrowserRouter>
+  <StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Landing />} />
+        <Route element={<AuthLayout />}>
+          <Route path="signup" element={<SignUp />} />
+          <Route path="signin" element={<SignIn />} />
+          <Route path="forgotpassword" element={<ForgotPassord />} />
+        </Route>
+        <Route element={<Layout />}>
+          <Route path="home" element={<Home />} />
+          <Route path="servers" element={<Servers />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
 )
